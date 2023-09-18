f9e4538b1a4baf6442338657b007f8dc69ce86a4
Stavromir <s.kasapov@protonmail.com>
2023-09-18 21:44:13 +0300

    Create new topic folder and tasks

---
+++ src/P05_MultiDimensopnalArrays/LABS/T01_CompareMatrices.java
@@ -0,0 +1,9 @@
+package P05_MultiDimensopnalArrays.LABS;
+
+import java.util.Scanner;
+
+public class T01_CompareMatrices {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
