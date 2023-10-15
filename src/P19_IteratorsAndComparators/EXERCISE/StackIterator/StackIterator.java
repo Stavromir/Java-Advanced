839feb7245dc2ac3bf47c17af1ce1047a5643915
Stavromir <s.kasapov@protonmail.com>
2023-10-15 12:54:11 +0300

    Update StackIterator.java

--- src/P19_IteratorsAndComparators/EXERCISE/StackIterator/StackIterator.java
+++ src/P19_IteratorsAndComparators/EXERCISE/StackIterator/StackIterator.java
@@ -1,4 +1,46 @@
 package P19_IteratorsAndComparators.EXERCISE.StackIterator;
 
-public class StackIterator {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class StackIterator<T> implements Iterable<T>{
+
+    private List<T> data;
+
+    public StackIterator() {
+        this.data = new ArrayList<>();
+    }
+
+    public void push (T element) {
+        this.data.add(element);
+    }
+
+    public void pop () {
+        if (this.data.isEmpty()) {
+            throw new IllegalStateException("No elements");
+        } else {
+            this.data.remove(this.data.size() - 1);
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new CustomIterator();
+    }
+
+    private class CustomIterator implements Iterator<T> {
+
+        int index = data.size() - 1;
+
+        @Override
+        public boolean hasNext() {
+            return index >= 0;
+        }
+
+        @Override
+        public T next() {
+            return data.get(index--);
+        }
+    }
 }
